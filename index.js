b0efab0b990b31a4afacec80797cac8d62f1f862
Vinit Gupta <vinitman35@gmail.com>
2021-08-20 17:28:29 +0530

    hide menu and add menu open effect

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let menuList = document.querySelectorAll(".menu-list");
-console.log(menuList)
+let navbar = document.querySelector(".navbar");
+let menuIcon = document.querySelector(".menu-icon");
+//console.log(navbar.classList.add("navbar-hidden"))
+if(window.innerWidth < 950){
+    navbar.style.left = "-46vw";
+}
+console.log(menuList,typeof window.innerWidth)
 menuList.forEach((listItem,index)=>{
     let opacity = 1 - (index+1)/10;
     listItem.style.backgroundColor = `rgb(2, 87, 132,${opacity})`;
@@ -8,3 +14,10 @@ menuList.forEach((listItem,index)=>{
         console.log("last",listItem.classList)
     }
 })
+
+menuIcon.addEventListener("click",openNav)
+
+function openNav() {
+    console.log("Menu Open");
+    navbar.style.left = "0";
+}
